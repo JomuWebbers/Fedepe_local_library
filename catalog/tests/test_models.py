24dc309a671e87e38176b2ab189bd2ae076d7abc
JomuWebbers <samueljomu@gmail.com>
2025-12-19 04:02:03 +0100

    Remove stray placeholder catalog/tests.py to avoid conflict with catalog.tests package

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -2,11 +2,12 @@
 from django.test import TestCase
 from catalog.models import Author
 
+
+
+
+
+
 class AuthorModelTest(TestCase):
-
-
-
-
     @classmethod
     def setUpTestData(cls):
         Author.objects.create(first_name='John', last_name='Doe')
@@ -25,7 +26,26 @@ class AuthorModelTest(TestCase):
         author = Author.objects.get(id=1)
         expected_name = f'{author.last_name}, {author.first_name}'
         self.assertEqual(str(author), expected_name)
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
 
+    def setUp(self):
+        print("setUp: Run once for every test method to set up clean data.")
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(False)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
 
 
         
