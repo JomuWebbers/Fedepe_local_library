f58e0b799ba233e8713da884750b0840a1a82cde
JomuWebbers <samueljomu@gmail.com>
2025-12-23 06:36:32 +0100

    Pin deployment package versions (keep Django 5.2.5)

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -1,39 +1,37 @@
 
+import datetime
+
 from django.test import TestCase
-from catalog.models import Author
+from django.utils import timezone
 
+from catalog.forms import RenewBookForm
 
+class RenewBookFormTest(TestCase):
+    def test_renew_form_date_field_label(self):
+        form = RenewBookForm()
+        self.assertTrue(form.fields['renewal_date'].label is None or form.fields['renewal_date'].label == 'renewal date')
 
+    def test_renew_form_date_field_help_text(self):
+        form = RenewBookForm()
+        self.assertEqual(form.fields['renewal_date'].help_text, 'Enter a date between now and 4 weeks (default 3).')
 
+    def test_renew_form_date_in_past(self):
+        date = datetime.date.today() - datetime.timedelta(days=1)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertFalse(form.is_valid())
 
+    def test_renew_form_date_too_far_in_future(self):
+        date = datetime.date.today() + datetime.timedelta(weeks=4) + datetime.timedelta(days=1)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertFalse(form.is_valid())
 
-class AuthorModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        Author.objects.create(first_name='John', last_name='Doe')
+    def test_renew_form_date_today(self):
+        date = datetime.date.today()
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertTrue(form.is_valid())
 
-    def test_first_name_label(self):
-        author = Author.objects.get(id=1)
-        field_label = author._meta.get_field('first_name').verbose_name
-        self.assertEqual(field_label, 'first name')
-
-    def test_first_name_max_length(self):
-        author = Author.objects.get(id=1)
-        max_length = author._meta.get_field('first_name').max_length
-        self.assertEqual(max_length, 100)
-
-    def test_object_name(self):
-        author = Author.objects.get(id=1)
-        expected_name = f'{author.last_name}, {author.first_name}'
-        self.assertEqual(str(author), expected_name)
-
-    def test_false_is_false(self):
-        self.assertFalse(False)
-
-    def test_false_is_true(self):
-        self.assertTrue(True)
-
-    def test_one_plus_one_equals_two(self):
-        self.assertEqual(1 + 1, 2)
-
+    def test_renew_form_date_max(self):
+        date = timezone.localtime() + datetime.timedelta(weeks=4)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertTrue(form.is_valid())
 
